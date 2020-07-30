24ec9410e36b06c48b60679e95b2f103514d06ba
soongwei2 <soongwei2@gmail.com>
2020-07-30 22:15:35 +0800

    no message

--- services/torrent-api/thepiratebay.js
+++ services/torrent-api/thepiratebay.js
@@ -1,71 +1,79 @@
 const axios = require('axios');
 const config = require('config');
 const pretty = require('prettysize');
+const errorCodes = require('../../utilities/handler/error.codes');
+const { parameterHandler } = require('../../utilities/handler/errorDef');
 
 module.exports = {
   get(type = 0, search = '') {
-    switch (type) {
-      case '0': //search
-        console.log("here")
+    switch (Number.parseInt(type)) {
+      case 0: //search
         return searchAPI(search);
-      case '1': //top movies
-
-        break;
-      case '2': //top tv shows
-
-        break;
-
-      case '3': //toop music
-
-        break;
+      case 1: //top movies
+        return top100API(config.domains.thepiratebay.top100movies);
+      case 2: //top tv shows
+        return top100API(config.domains.thepiratebay.top100tvshows);
+      case 3: //toop music
+        return top100API(config.domains.thepiratebay.top100music);
+      default:
+        throw errorCodes.INVALID_PARAMETER;
     }
   }
 }
 
 
-//https://apibay.org/q.php?q=incredible%20hulk&cat=
 function searchAPI(search = '') {
-  console.log("search2: ", search)
-  return axios.get(config.domains.thepiratebay, {
+  return axios.get(config.domains.thepiratebay.search, {
       params: {
         q: search,
         cat: '',
       }
     })
     .then(function (response) {
-      console.log("response: ", response.data);
       return response.data.filter(x => x.id != 0).map((eachData) => {
-        return {
-          title: eachData.name,
-          magnet: '',
-          age: new Date(eachData.added * 1000).toLocaleDateString(undefined, {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-          }),
-          seed: eachData.seeders,
-          size: pretty(eachData.size, true),
-          path: '',
-          path2: '',
-        }
+        return parseEachTorrent(eachData);
       });
-    })
+    });
+}
 
-  function getPath(category = 0) {
-    if (category == 201 ||
-      category == 202 ||
-      category == 204 ||
-      category == 207) {
-      return 'Movies';
-    } else if (category == 205 ||
-      category == 208) {
-      return 'TV Shows';
-    } else if (category == 101 ||
-      category == 203) {
-      return 'Music';
-    }else{
-      return 'Others';
-    }
+function top100API(url) {
+  return axios.get(url)
+    .then(function (response) {
+      return response.data.filter(x => x.id != 0).map((eachData) => {
+        return parseEachTorrent(eachData);
+      });
+    });
+}
+
+function parseEachTorrent(eachData) {
+  return {
+    title: eachData.name,
+    magnet: 'magnet:?xt=urn:btih:' + eachData.info_hash + '&dn=' + encodeURIComponent(eachData.name),
+    age: new Date(eachData.added * 1000).toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    }),
+    seed: eachData.seeders,
+    size: pretty(eachData.size, true),
+    path: '',
+    path2: getPath(eachData.category),
   }
+}
 
+function getPath(category = 0) {
+  if (category == 201 ||
+    category == 202 ||
+    category == 204 ||
+    category == 207) {
+    return 'Movies';
+  } else if (category == 205 ||
+    category == 208) {
+    return 'TV Shows';
+  } else if (category == 101 ||
+    category == 203) {
+    return 'Music';
+  } else {
+    return 'Others';
+  }
 }
